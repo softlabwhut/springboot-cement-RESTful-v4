55b28d19361620a9f78e8833d651997be377faae
danielwu927 <danielwu927@outlook.com>
2018-06-02 12:05:01 +0800

    完善mybatis配置

--- src/main/java/com/whut/springbootcementrestulv4/dao/UserMapper.java
+++ src/main/java/com/whut/springbootcementrestulv4/dao/UserMapper.java
@@ -1,4 +1,7 @@
 package com.whut.springbootcementrestulv4.dao;
 
+import org.mybatis.spring.annotation.MapperScan;
+
+@MapperScan   //@Mapper或者@Mapperscan 把接口扫描装配到容器
 public interface UserMapper {
 }
